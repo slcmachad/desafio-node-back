be736b522e85182a9eebd0a302b7c4a47deafde1
Salomao <salomao.machado.75@gmail.com>
2023-11-05 14:34:47 -0300

    Feat: adicionando elementos de segurança para autenticação.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+
+app.get('/', (req, res) => {
+    res.status(200).json({msg: 'funciona pplease'});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("funfa?");
+});
 
