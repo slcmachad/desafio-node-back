170d611f0b081ba4dc43c14e677d3bc24a76b3db
Salomao <salomao.machado.75@gmail.com>
2023-11-05 22:25:18 -0300

    refactor: refatorando parte de autenticação para os arquivos de destino

--- app.js
+++ app.js
@@ -7,156 +7,45 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 const database = require('./model/database');
-//const auth = require('./model/auth');
+const auth = require('./model/auth');
 
-//models
+// Models
 const User = require('./model/User');
 
-//conecção com banco de dados
+// Conexão com o banco de dados
 database.connect();
 
-//configurar resposta json
+// Configurar resposta JSON
 app.use(express.json());
 
-//rotas publicas
+// Rotas públicas
 app.get('/', (req, res) => {
-    res.status(200).json({msg: 'bem vindo'});
-})
-
-//rotas privadas
-app.get("/users/:id", checkToken, async (req, res) => {
-    const id = req.params.id
-
-    //checar se tem esse usuario
-
-    const user = await User.findById(id, '-password')
-
-    if(!user){
-        return res.status(404).json({msg: "Usuário não encontrado"})
-    }
-
-    res.status(200).json({ user })
-
-})
-
-function checkToken(req, res, next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if(!token){
-        return res.status(401).json({msg: "Acesso negado(deve ta de hack)"})
-    }
-
-    try {
-        const secret = process.env.JWT_SECRET;
-
-        jwt.verify(token, secret);
-
-        next();
-    } catch (error) {
-        res.status(400).json({msg: "Token inválido"})
-    }
-}
-
-//para registro de usuarios
-app.post('/auth/register', async(req, res) => {
-    const {nome, email, cpf,password, confirmPassword} = req.body
-
-    //validações
-
-    if(!nome){
-        return res.status(422).json({msg: "O nome é obrigatório"});
-    }
-    if(!email){
-        return res.status(422).json({msg: "O email é obrigatório"});
-    }
-    if(!cpf){
-        return res.status(422).json({msg: "O cpf é obrigatório"});
-    }
-    if(!password){
-        return res.status(422).json({msg: "A senha é obrigatória"});
-    }
-    if(password !== confirmPassword){
-        return res.status(422).json({msg: "As senhas devem ser identicas"});
-    }
-
-    //checar se já existe usuario
-
-    const userExists = await User.findOne({cpf: cpf})
-
-    if(userExists){
-        return res.status(422).json({msg: "Usuário já cadastrado com esse cpf"});
-    }
-
-    // Criar a senha
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    // Criar usuario
-    const user = new User({
-        nome,
-        email,
-        cpf,
-        password: passwordHash,
-    })
-
-    try{
-        await user.save();
-        res.status(201).json({msg: "funciona please"});
-    }catch(erro){
-        console.log(erro);
-        res.status(500).json({msg: "Houve um problema, por favor tente novamente em alguns minutos"})
-    }
-
-})
-
-// login user
-
-app.post('/auth/login', async(req, res) => {
-    const {email, password} = req.body;
-
-    if(!email){
-        return res.status(422).json({msg: "O email é obrigatório"});
-    }
-    
-    if(!password){
-        return res.status(422).json({msg: "A senha é obrigatória"});
-    }
-
-    //verificar se já tem usuario
-
-    const user = await User.findOne({email: email})
-
-    if(!user){
-        return res.status(404).json({msg: "Usuário Não encontrado"});
-    }
-
-    //verificar se senhas são compativeis(se ele não ta de hack)
-    const checkPassword = await bcrypt.compare(password, user.password)
-
-    if(!checkPassword){
-        return res.status(422).json({msg: "Senha inválida"});
-    }
-
-    try{
-        const secret = process.env.JWT_SECRET;
-        
-        const token = jwt.sign({
-            id: user._id
-        }, secret)
-
-        res.status(200).json({msg: "autenticado com sucesso", token})
-
-    }catch(erro){
-        console.log(erro);
-        res.status(500).json({msg: "Houve um problema, por favor tente novamente em alguns minutos"})
-    }
-})
-
-
-
-app.listen(process.env.PORT, () => {
-    console.log("servidor iniciado");
+  res.status(200).json({ msg: 'Bem-vindo' });
 });
 
+// Rotas privadas
+app.get("/users/:id", auth.checkToken, async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const user = await User.findById(id, '-password');
+
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: "Erro ao buscar usuário" });
+  }
+});
+
+// Registro de usuários
+app.post('/auth/register', auth.registerUser);
+
+// Login de usuários
+app.post('/auth/login', auth.loginUser);
+
+app.listen(process.env.PORT, () => {
+  console.log("Servidor iniciado");
+});
--- model/auth.js
+++ model/auth.js
@@ -1,28 +1,116 @@
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('./User');
 
-const secretKey = process.env.JWT_SECRET;
 
-function geradorDeTokens(dadosUsuario){
-    return jwt.sign({id: dadosUsuario._id}, secretKey, {expiresIn: '24h'});
+//função para validar o token
+function checkToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado (deve estar hackeando)" });
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Token inválido" });
+  }
 }
 
-function verificarToken(req, res, next){
-    const token = req.headers.authorization;
+//função para registrar o ususario
+async function registerUser(req, res) {
+  const { nome, email, cpf, password, confirmPassword } = req.body;
 
-    if(!token){
-        return res.status(401).json({message: "token não existente"});
-    }
-    jwt.verify(token, secretKey, (erro, dadosUsuario) => {
-        if(erro){
-            return res.status(403).json({message: 'Token invalido'});
-        }
+  // Validações
 
-        req.dadosUsuario = dadosUsuario;
-        next();
-    })
+  if (!nome) {
+    return res.status(422).json({ msg: "O nome é obrigatório" });
+  }
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório" });
+  }
+  if (!cpf) {
+    return res.status(422).json({ msg: "O cpf é obrigatório" });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória" });
+  }
+  if (password !== confirmPassword) {
+    return res.status(422).json({ msg: "As senhas devem ser idênticas" });
+  }
+
+  // Verificar se já existe um usuário com o mesmo CPF
+  const userExists = await User.findOne({ cpf: cpf });
+
+  if (userExists) {
+    return res.status(422).json({ msg: "Usuário já cadastrado com esse CPF" });
+  }
+
+  // Criar a senha
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Criar usuário
+  const user = new User({
+    nome,
+    email,
+    cpf,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+    res.status(201).json({ msg: "Usuário cadastrado com sucesso" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Houve um problema, por favor, tente novamente em alguns minutos" });
+  }
+}
+
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória" });
+  }
+
+  // Verificar se o usuário com o email fornecido existe
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado" });
+  }
+
+  // Verificar se as senhas são compatíveis
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida" });
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET;
+    const token = jwt.sign({
+      id: user._id
+    }, secret);
+
+    res.status(200).json({ msg: "Autenticado com sucesso", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Houve um problema, por favor, tente novamente em alguns minutos" });
+  }
 }
 
 module.exports = {
-    geradorDeTokens,
-    verificarToken
-}
+  checkToken,
+  registerUser,
+  loginUser,
+};
