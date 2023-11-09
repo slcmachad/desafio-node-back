3790b9386e462f47ca383bd1ceb3caa12d8033ce
Salomao <salomao.machado.75@gmail.com>
2023-11-08 21:29:40 -0300

    Feat: Criando Logica para a adição de turmas e verificação de conflitos com professores.

--- model/professor.js
+++ model/professor.js
@@ -12,16 +12,53 @@ function checkProfessorRole(req, res, next) {
         }
 }
 router.get('/turmas', auth.checkToken, checkProfessorRole, async (req, res) => {
-        // Lógica para professores verem as turmas disponíveis
-        // ...
-        res.status(200).json({ msg: 'Bem-vindo, professor' });
+        try {
+                const turmas = await Turma.find({});
+                res.status(200).json({turmas});
+        } catch (erro) {
+                console.error(erro);
+                res.status(500).json({msg: 'Houve um problema ao buscar as turmas'})
+        }
 });
 
 router.post('/criarTurmas', auth.checkToken, checkProfessorRole, async (req, res) => {
-        // Lógica para professores criarem turmas
-        // ...
-        res.status(200).json({ msg: 'Turma criada com sucesso' });
-});
+        try{
+                //criando o json para a turma
+                const {horario, diaDaSemana, capacidade, disciplina, professorId, alunos} = req.body;
 
+                //validando horario e dia
+                if(!['manha', 'tarde'].includes(horario)){
+                        return res.status(400).json({msg: 'Horário inválido, somente pelas manhãs e tardes'});
+                }
+
+                if(!['segunda', 'quarta', 'sexta'].includes(diaDaSemana)){
+                        return res.status(400).json({msg: 'Dia da semana inválido. Deve ser "segunda", "quarta" ou "sexta".'});
+                }
+
+                //verificando se existe já para esse professor
+                const turmaExiste = await Turma.findOne({
+                        diaDaSemana: diaDaSemana,
+                        horario: horario,
+                        professor: professorId
+                })
+
+                if(turmaExiste){
+                        return res.status(400).json({msg: "Este professor Ja está ocupado neste dia e horário"});
+                }
+                //procurando professor
+                const professor = await User.findById(professorId);
+
+                if(!professor){
+                        return res.status(404).json({msg: "Professor não encontrado"});
+                }
+                //criando a turma e salvando no db
+                const turma = new Turma({horario, diaDaSemana, capacidade, disciplina, professor: professor._id, alunos});
+                await turma.save();
+                res.status(201).json({msg: "Turma criada com sucesso."});
+        }catch(erro){
+                console.error(erro);
+                res.status(500).json({msg: "Houve um problema ao criar a turma"});
+        }
+});
 
 module.exports = router
